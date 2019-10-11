816e3f3f5000f7afd88203110eb33b649d3e7737
João Marques <joao@jjmf.com>
2019-10-10 22:22:37 -0300

    Frontend routes.

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ if (config.error) {
 // Load modules
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const helmet = require('helmet');
 const app = express();
@@ -18,6 +19,7 @@ const app = express();
 mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + '?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 app.use(helmet());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
