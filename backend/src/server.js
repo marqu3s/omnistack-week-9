5f9d39329f3a863c09f4934a95a268942b354574
João Marques <joao@jjmf.com>
2019-10-10 23:46:02 -0300

    Spot management.

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,8 +18,14 @@ const app = express();
 // Database Connection
 mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + '?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
+// Middlewares
 app.use(helmet());
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
+
+// Routes
+app.use('/spot-images', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
+
+// Start APP
 app.listen(3333);
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import './styles.css';
 
 export default function Dashboard() {
-    return <div />
+    const [spots, setSpots] = useState([]);
+
+    // Fetch data on page load
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+        }
+        loadSpots();
+    }, []); // execute 1 time only
+
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})`}} />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
+                    </li>
+                ))}
+            </ul>
+
+            <Link to="/new">
+                <button className="btn">Cadastrar novo spot</button>
+            </Link>
+        </>
+    );
 }
